51b0c74d3dbce9c5563cf9c1b87d9f16bdab5779
xm1ls <xm1ls2000@gmail.com>
2023-08-09 11:49:42 +0300

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,72 @@
+import css from './Header.module.css';
+
+export const Header = () => {
+    return (
+        <div className={css.container}>
+            <div className={`${css.item} ${css.span_column}`} />
+            <div className={`${css.item} ${css.span_row}`} />
+            <div className={`${css.item} ${css.span_column} ${css.span_row}`} />
+            <div className={css.item_container}>
+                <div className={`${css.item} ${css.span_column} ${css.span_row}`} />
+                <div className={css.item}  />
+                <div className={`${css.item} ${css.span_row}`} />
+                <div className={`${css.item} ${css.span_column}`} />
+                
+            </div>
+
+
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+
+        </div>
+    );
+}
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,111 @@
+import css from './Sidebar.module.css'
+
+export const Sidebar = () => {
+    return (
+        <div className={css.container}>
+            <div className={`${css.item} ${css.span_column}`} />
+            <div className={`${css.item} ${css.span_row}`} />
+            <div className={`${css.item} ${css.span_column} ${css.span_row}`} />
+            <div className={css.item_container}>
+                <div className={`${css.item} ${css.span_column} ${css.span_row}`} />
+                <div className={css.item}  />
+                <div className={`${css.item} ${css.span_row}`} />
+                <div className={`${css.item} ${css.span_column}`} />
+                
+            </div>
+
+
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+            <div className={css.item}/>
+        </div>
+    );
+}
