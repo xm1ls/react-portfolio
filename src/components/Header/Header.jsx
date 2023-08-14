5345e18607a252fee1b36b603f9e41eb69251fd4
xm1ls <xm1ls2000@gmail.com>
2023-08-14 21:50:53 +0300

    added NavLinks

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,27 @@
+import { NavLink } from 'react-router-dom';
 import css from './Header.module.css';
 
 export const Header = () => {
     return (
         <div className={css.outer_container}>
             <div className={css.header}>
-                <a href='/about' >
+                <NavLink to='/about' className={navData => navData.isActive ? css.active : css.a}>
                     <p className={css.about}>
                         /about.
                     </p>
-                </a>
+                </NavLink>
 
-                <a href='/projects'>
+                <NavLink to='/projects' className={navData => navData.isActive ? css.active : css.a}>
                     <p className={css.projects}>
                         /projects.
                     </p>
-                </a>
+                </NavLink>
 
-                <a href='/contacts'>
+                <NavLink to='/contacts' className={navData => navData.isActive ? css.active : css.a}>
                     <p className={css.contacts}>
                         /contacts.
                     </p>
-                </a>
+                </NavLink>
             </div>
         </div>
 
